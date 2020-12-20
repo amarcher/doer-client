b095a242d77480337ea0b64b027d61172d45f45e
Andrew Archer <andrew.m.archer@gmail.com>
2020-12-20 10:54:32 -0500

    add apollo

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  ApolloClient,
+  NormalizedCacheObject,
+  ApolloProvider,
+} from '@apollo/client';
+
+import { cache } from './cache';
+
+// Set up our apollo-client to point at our server
+// this can be local or a remote endpoint
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  cache,
+  uri: 'http://localhost:4000/graphql',
+  headers: {
+    authorization: localStorage.getItem('token') || '',
+    'client-name': 'Doer [Web]',
+    'client-version': '1.0.0',
+  },
+  resolvers: {},
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
